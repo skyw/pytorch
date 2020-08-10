6018383e3a60601a3655b3544f945d106869ad6f
Hao Wu <skyw@nvidia.com>
2020-08-10 15:42:37 -0700

    Add import parse_args back

--- torch/onnx/symbolic_opset13.py
+++ torch/onnx/symbolic_opset13.py
@@ -1,5 +1,7 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+from torch.onnx.symbolic_helper import parse_args
+
 @parse_args('v', 'v', 'v', 'i', 'i', 'i')
 def fake_quantize_per_channel_affine(g, inputs, scale, zero_point, axis, quant_min=-128, quant_max=127):
     if quant_min not in [0, -128] or quant_max not in [127, 255]:
