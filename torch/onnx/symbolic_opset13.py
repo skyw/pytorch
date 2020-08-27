583b5cecf4641bef3830b5c227ca99a59669de18
Hao Wu <skyw@nvidia.com>
2020-08-26 20:31:55 -0700

    convert zero_point to 8bit type when exporting to onnx

--- torch/onnx/symbolic_opset13.py
+++ torch/onnx/symbolic_opset13.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-from torch.onnx.symbolic_helper import parse_args
+from torch.onnx.symbolic_helper import parse_args, cast_pytorch_to_onnx
 
 @parse_args('v', 'v', 'v', 'i', 'i', 'i')
 def fake_quantize_per_channel_affine(g, inputs, scale, zero_point, axis, quant_min=-128, quant_max=127):
@@ -8,6 +8,11 @@ def fake_quantize_per_channel_affine(g, inputs, scale, zero_point, axis, quant_m
         raise RuntimeError(
             "ONNX defines [0, 255] for quint8 and [-128, 127] for qint8, got [{}, {}]".format(quant_min, quant_max))
 
+    # ONNX defines zero_point to be int8 or uint8
+    if quant_min == 0:
+        zero_point = g.op("Cast", zero_point, to_i=cast_pytorch_to_onnx['Byte'])
+    else:
+        zero_point = g.op("Cast", zero_point, to_i=cast_pytorch_to_onnx['Char'])
     return g.op(
         "DequantizeLinear",
         g.op("QuantizeLinear", inputs, scale, zero_point, axis_i=axis),
